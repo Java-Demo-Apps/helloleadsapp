de1ca460025bee82a8bd9e3392add807188b2a80
Nimisha Vaghela <nimivaghela@gmail.com>
2024-12-06 17:19:36 +0530

    Initial commit

---
+++ src/main/java/com/nimisha/helloleadsapp/HelloleadsappApplication.java
@@ -0,0 +1,13 @@
+package com.nimisha.helloleadsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloleadsappApplication {
+
+    public static void main(String[] args)   {
+        SpringApplication.run(HelloleadsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nimisha/helloleadsapp/HelloleadsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimisha.helloleadsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloleadsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
